0b04b3c0ebd6e5f2c2e9071f810c23094ba1fa83
muhreynaldii <reynaldi180101@gmail.com>
2023-06-08 22:27:44 +0700

    fix url mongo

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise;
 
 async function connectToDatabase() {
   try {
-    const connectionString = `mongodb://localhost:27017/moodsync_backend&replicaSet=rs0`;
+    const connectionString = `mongodb+srv://reynaldi:moodsync@cluster0.genin59.mongodb.net/?retryWrites=true&w=majority`;
     await mongoose.connect(connectionString, {
       serverSelectionTimeoutMS: 5000,
     });
