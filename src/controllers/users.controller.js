d31a5836f9689c1412c1fbe26885e71f1480b55f
derrydwi <derrydwiaditya@gmail.com>
2023-07-03 15:44:50 +0700

    feat: add overview & summary endpoint by userId

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,7 @@
 const { errorHandler } = require("../util");
 const models = require("../models");
 const { HttpError } = require("../error");
+const mongoose = require("mongoose");
 
 const getIds = errorHandler(async (req, res) => {
   const usernames = req.query.usernames;
@@ -33,6 +34,122 @@ const getById = errorHandler(async (req, res) => {
     throw new HttpError(400, "User not found");
   }
   return userDoc;
+});
+
+const getOverview = errorHandler(async (req, res) => {
+  const data = await models.Recognition.aggregate([
+    {
+      $match: {
+        userId: mongoose.Types.ObjectId(req.params.id),
+        meetingId: {
+          $in: await models.Meeting.find({}).distinct("_id"),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        neutral: { $avg: "$neutral" },
+        happy: { $avg: "$happy" },
+        sad: { $avg: "$sad" },
+        angry: { $avg: "$angry" },
+        fearful: { $avg: "$fearful" },
+        disgusted: { $avg: "$disgusted" },
+        surprised: { $avg: "$surprised" },
+      },
+    },
+    {
+      $project: {
+        neutral: { $round: { $multiply: ["$neutral", 100] } },
+        happy: { $round: { $multiply: ["$happy", 100] } },
+        sad: { $round: { $multiply: ["$sad", 100] } },
+        angry: { $round: { $multiply: ["$angry", 100] } },
+        fearful: { $round: { $multiply: ["$fearful", 100] } },
+        disgusted: { $round: { $multiply: ["$disgusted", 100] } },
+        surprised: { $round: { $multiply: ["$surprised", 100] } },
+      },
+    },
+    { $unset: ["_id"] },
+  ]);
+  const labels = [
+    "Neutral",
+    "Happy",
+    "Sad",
+    "Angry",
+    "Fearful",
+    "Disgusted",
+    "Surprised",
+  ];
+  if (!data.length) {
+    throw new HttpError(400, "User not found");
+  }
+  return { labels, datas: Object.values(data[0]) };
+});
+
+const getSummary = errorHandler(async (req, res) => {
+  const data = await models.Recognition.aggregate([
+    {
+      $match: {
+        userId: mongoose.Types.ObjectId(req.params.id),
+        meetingId: {
+          $in: await models.Meeting.find({}).distinct("_id"),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        positive: { $sum: { $add: ["$happy", "$surprised"] } },
+        negative: {
+          $sum: { $add: ["$sad", "$angry", "$fearful", "$disgusted"] },
+        },
+        count: {
+          $sum: {
+            $add: [
+              "$happy",
+              "$sad",
+              "$angry",
+              "$fearful",
+              "$disgusted",
+              "$surprised",
+            ],
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        positive: {
+          $cond: [
+            { $eq: ["$count", 0] },
+            0,
+            {
+              $round: {
+                $multiply: [{ $divide: ["$positive", "$count"] }, 100],
+              },
+            },
+          ],
+        },
+        negative: {
+          $cond: [
+            { $eq: ["$count", 0] },
+            0,
+            {
+              $round: {
+                $multiply: [{ $divide: ["$negative", "$count"] }, 100],
+              },
+            },
+          ],
+        },
+      },
+    },
+    { $unset: ["_id", "count"] },
+  ]);
+  const labels = ["Positive", "Negative"];
+  if (!data.length) {
+    throw new HttpError(400, "User not found");
+  }
+  return { labels, datas: Object.values(data[0]) };
 });
 
 const me = errorHandler(async (req, res) => {
@@ -69,4 +186,6 @@ module.exports = {
   postNewUsers,
   getAllUsers,
   getById,
+  getOverview,
+  getSummary,
 };
