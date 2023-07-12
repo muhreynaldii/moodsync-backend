3179038e1011c306026fa46749c9eb319399672b
derrydwi <derrydwiaditya@gmail.com>
2023-07-12 07:33:33 +0700

    refactor: use ov REST API instead of ov client

--- src/routes/session.js
+++ src/routes/session.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.post("/", controllers.session.create);
 router.post("/:sessionId/connections", controllers.session.createConnection);
+router.delete("/:sessionId", controllers.session.removeSession);
+router.delete("/:sessionId/connection/:connectionId", controllers.session.removeConnection);
 
 module.exports = router;
